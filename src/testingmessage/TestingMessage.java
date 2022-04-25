c1dbb2dd9215c1b10d69ac081c87d41757b8e68a
Jaden <st10070274@vcconnect.edu.za>
2022-04-25 14:12:08 +0200

    init commit

---
+++ src/testingmessage/TestingMessage.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testingmessage;
+
+/**
+ *
+ * @author ST10070274
+ */
+public class TestingMessage {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    String returnMessage = "Hello World";
+    
+    
+    public String getMessage()
+    {
+        return returnMessage;
+    }
+    
+    
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+    
+}
